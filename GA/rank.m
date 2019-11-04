aefdcffb23e9e7975929cfbbc67bb61cc20646b5
wr_Han <sofiahwr@gmail.com>
2019-11-04 11:20:35 -0500

    rank

--- GA/rank.m
+++ GA/rank.m
@@ -2,49 +2,47 @@
 
 function rank(population_size, chromo_size)
 global fitness_value;   
-global fitness_sum;     % accumulated population fitness value
+global fitness_sum;     
 global fitness_average;
 global best_fitness;
 global best_individual;
 global best_generation;
-global population;
 global G;
+global population_new;
 
-for i=1:population_size    
-    fitness_sum(i) = 0.;
+for i=1:(2.5*population_size)
+    fitness_sum(i) = 0;
 end
 
-max_index = 1;
-temp = 1;
+
 temp_chromosome(chromo_size)=0;
 
-
-for i=1:population_size
+for i=1:(2.5*population_size)
     max_index = i;
-    for j = i+1:population_size
+    for j = i+1:(2.5*population_size)
         if fitness_value(j) > fitness_value(max_index)
             max_index = j;
         end
     end
     
     if max_index ~= i
-        % exchange fitness_value(i) and fitness_value(min_index)
+        % exchange fitness_value(i) and fitness_value(max_index)
         temp = fitness_value(i);
         fitness_value(i) = fitness_value(max_index);
         fitness_value(max_index) = temp;
         
         
-        % exchange population(i) and population(min_index)
+        % exchange population_new(i) and population_new(max_index)
         for k = 1:chromo_size
-            temp_chromosome(k) = population(i,k);
-            population(i,k) = population(max_index,k);
-            population(max_index,k) = temp_chromosome(k);
+            temp_chromosome(k) = population_new(i,k);
+            population_new(i,k) = population_new(max_index,k);
+            population_new(max_index,k) = temp_chromosome(k);
         end
     end
 end
 
 % fitness_sum
-for i=1:population_size
+for i=1:(2.5*population_size)
     if i==1
         fitness_sum(i) = fitness_sum(i) + fitness_value(i);    
     else
@@ -53,14 +51,14 @@ for i=1:population_size
 end
 
 % fitness_average(G) = average fitness at G's generation
-fitness_average(G) = fitness_sum(population_size)/population_size;
+fitness_average(G) = fitness_sum(2.5*population_size)/(2.5*population_size);
 
 %
-if fitness_value(population_size) < best_fitness
-    best_fitness = fitness_value(population_size);
+if fitness_value(2.5*population_size) < best_fitness
+    best_fitness = fitness_value(2.5*population_size);
     best_generation = G;
     for j=1:chromo_size
-        best_individual(j) = population(population_size,j);
+        best_individual(j) = population_new(2.5*population_size,j);
     end
 end
 
